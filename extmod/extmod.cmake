584e13740a42ec395b77620ab7d5a306b5b59626
kons <lorenzo.consolaro@tiko.energy>
2021-09-05 18:01:10 +0200

    [wolfSSL] Implement self signed certificate generation

--- extmod/extmod.cmake
+++ extmod/extmod.cmake
@@ -29,6 +29,7 @@ set(MICROPY_SOURCE_EXTMOD
     ${MICROPY_EXTMOD_DIR}/modussl_axtls.c
     ${MICROPY_EXTMOD_DIR}/modussl_mbedtls.c
     ${MICROPY_EXTMOD_DIR}/modussl_wolfssl.c
+    ${MICROPY_EXTMOD_DIR}/modussl_wolfssl_cert.c
     ${MICROPY_EXTMOD_DIR}/modutimeq.c
     ${MICROPY_EXTMOD_DIR}/moduwebsocket.c
     ${MICROPY_EXTMOD_DIR}/moduzlib.c
